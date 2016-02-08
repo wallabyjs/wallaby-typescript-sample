9a86a273364712036524a9c49c763113f05cf1cc
Artem Govorov <artem.govorov@gmail.com>
2016-02-08 15:39:20 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -8,12 +8,5 @@ module.exports = function (w) {
     tests: [
       'test/*BrowserSpec.ts'
     ]
-
-    // TypeScript compiler is on by default with default options (and options from tsconfig.json),
-    // you can also configure built-in compiler by passing options to it
-    //compilers: {
-    //  '**/*.ts': w.compilers.typeScript({})
-    //}
-
   };
 };
